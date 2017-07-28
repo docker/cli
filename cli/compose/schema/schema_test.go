468a665e58531652c9fe9c4a9baad4e89264548b
Ali Al-Shabibi <alshabibi.ali@gmail.com>
2017-07-28 15:42:26 -0700

    Adding support for Rollback config in compose 3.4
    Signed-off-by: Ali Al-Shabibi <alshabibi.ali@gmail.com>

--- cli/compose/schema/schema_test.go
+++ cli/compose/schema/schema_test.go
@@ -74,3 +74,92 @@ func TestValidatePlacement(t *testing.T) {
 
 	assert.NoError(t, Validate(config, "3.3"))
 }
+
+func TestValidateRollbackConfig(t *testing.T) {
+	config := dict{
+		"version": "3.4",
+		"services": dict{
+			"foo": dict{
+				"image": "busybox",
+				"deploy": dict{
+					"rollback_config": dict{
+						"parallelism": 1,
+					},
+				},
+			},
+		},
+	}
+
+	assert.NoError(t, Validate(config, "3.4"))
+}
+
+func TestValidateRollbackConfigWithOrder(t *testing.T) {
+	config := dict{
+		"version": "3.4",
+		"services": dict{
+			"foo": dict{
+				"image": "busybox",
+				"deploy": dict{
+					"rollback_config": dict{
+						"parallelism": 1,
+						"order":       "start-first",
+					},
+				},
+			},
+		},
+	}
+
+	assert.NoError(t, Validate(config, "3.4"))
+}
+
+func TestValidateRollbackConfigWithUpdateConfig(t *testing.T) {
+	config := dict{
+		"version": "3.4",
+		"services": dict{
+			"foo": dict{
+				"image": "busybox",
+				"deploy": dict{
+					"update_config": dict{
+						"parallelism": 1,
+						"order":       "start-first",
+					},
+					"rollback_config": dict{
+						"parallelism": 1,
+						"order":       "start-first",
+					},
+				},
+			},
+		},
+	}
+
+	assert.NoError(t, Validate(config, "3.4"))
+}
+
+func TestValidateRollbackConfigWithUpdateConfigFull(t *testing.T) {
+	config := dict{
+		"version": "3.4",
+		"services": dict{
+			"foo": dict{
+				"image": "busybox",
+				"deploy": dict{
+					"update_config": dict{
+						"parallelism":    1,
+						"order":          "start-first",
+						"delay":          "10s",
+						"failure_action": "pause",
+						"monitor":        "10s",
+					},
+					"rollback_config": dict{
+						"parallelism":    1,
+						"order":          "start-first",
+						"delay":          "10s",
+						"failure_action": "pause",
+						"monitor":        "10s",
+					},
+				},
+			},
+		},
+	}
+
+	assert.NoError(t, Validate(config, "3.4"))
+}
