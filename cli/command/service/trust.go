5849b18fcbfb8729338441e764b45c9b2237ca32
Sébastien HOUZÉ <cto@verylastroom.com>
2017-09-12 10:20:26 +0200

    Fix notary revendor bc breaks
    Signed-off-by: Sébastien HOUZÉ <cto@verylastroom.com>

--- cli/command/service/trust.go
+++ cli/command/service/trust.go
@@ -9,7 +9,6 @@ import (
 	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/registry"
 	"github.com/docker/notary/tuf/data"
-	"github.com/opencontainers/go-digest"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
@@ -64,13 +63,13 @@ func trustedResolveDigest(ctx context.Context, cli command.Cli, ref reference.Na
 		return nil, errors.Wrap(err, "error establishing connection to trust repository")
 	}
 
-	t, err := notaryRepo.GetTargetByName(ref.Tag(), trust.ReleasesRole, data.CanonicalTargetsRole)
+	t, err := notaryRepo.GetTargetByName(ref.Tag(), data.RoleName(trust.ReleasesRole), data.CanonicalTargetsRole)
 	if err != nil {
 		return nil, trust.NotaryError(repoInfo.Name.Name(), err)
 	}
 	// Only get the tag if it's in the top level targets role or the releases delegation role
 	// ignore it if it's in any other delegation roles
-	if t.Role != trust.ReleasesRole && t.Role != data.CanonicalTargetsRole {
+	if t.Role != data.RoleName(trust.ReleasesRole) && data.RoleName(t.Role) != data.CanonicalTargetsRole {
 		return nil, trust.NotaryError(repoInfo.Name.Name(), errors.Errorf("No trust data for %s", reference.FamiliarString(ref)))
 	}
 
