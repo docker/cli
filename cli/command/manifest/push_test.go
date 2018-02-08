dbd1fceddc41a97946282fe3570fc6c06e8c732f
Christy Norman Perez <christy@linux.vnet.ibm.com>
2018-02-07 20:21:58 -0600

    manifest push using yaml file
    Instead of:
    manifest create list image1 image2
    manifest annotate list image1
    manifest push list
    
    You can do:
    manifiest push --file myfile.yaml registry/repo/image:tag
    
    Signed-off-by: Christy Norman <christy@linux.vnet.ibm.com>

--- cli/command/manifest/push_test.go
+++ cli/command/manifest/push_test.go
@@ -15,10 +15,10 @@ import (
 func newFakeRegistryClient() *fakeRegistryClient {
 	return &fakeRegistryClient{
 		getManifestFunc: func(_ context.Context, _ reference.Named) (manifesttypes.ImageManifest, error) {
-			return manifesttypes.ImageManifest{}, errors.New("")
+			return manifesttypes.ImageManifest{}, errors.New("getManifestFunc not implemented")
 		},
 		getManifestListFunc: func(_ context.Context, _ reference.Named) ([]manifesttypes.ImageManifest, error) {
-			return nil, errors.Errorf("")
+			return nil, errors.Errorf("getManifestListFunc not implemented")
 		},
 	}
 }
@@ -66,4 +66,61 @@ func TestManifestPush(t *testing.T) {
 	cmd.SetArgs([]string{"example.com/list:v1"})
 	err = cmd.Execute()
 	assert.NilError(t, err)
+}
+
+func TestPushFromYaml(t *testing.T) {
+	cli := test.NewFakeCli(nil)
+	cli.SetRegistryClient(&fakeRegistryClient{
+		getManifestFunc: func(_ context.Context, ref reference.Named) (manifesttypes.ImageManifest, error) {
+			return fullImageManifest(t, ref), nil
+		},
+	})
+
+	cmd := newPushListCommand(cli)
+	cmd.Flags().Set("file", "testdata/test-push.yaml")
+	cmd.SetArgs([]string{"pushtest/pass:latest"})
+	assert.NilError(t, cmd.Execute())
+}
+
+func TestManifestPushYamlErrors(t *testing.T) {
+	testCases := []struct {
+		flags         map[string]string
+		args          []string
+		expectedError string
+	}{
+		{
+			flags:         map[string]string{"file": "testdata/test-push-fail.yaml"},
+			args:          []string{"pushtest/fail:latest"},
+			expectedError: "manifest entry for image has unsupported os/arch combination: linux/nope",
+		},
+		{
+			flags:         map[string]string{"file": "testdata/test-push-empty.yaml"},
+			args:          []string{"pushtest/fail:latest"},
+			expectedError: "no manifests specified in file input",
+		},
+		{
+			args:          []string{"testdata/test-push-empty.yaml"},
+			expectedError: "No such manifest: docker.io/testdata/test-push-empty.yaml:latest",
+		},
+	}
+
+	store, sCleanup := newTempManifestStore(t)
+	defer sCleanup()
+	for _, tc := range testCases {
+		cli := test.NewFakeCli(nil)
+		cli.SetRegistryClient(&fakeRegistryClient{
+			getManifestFunc: func(_ context.Context, ref reference.Named) (manifesttypes.ImageManifest, error) {
+				return fullImageManifest(t, ref), nil
+			},
+		})
+
+		cli.SetManifestStore(store)
+		cmd := newPushListCommand(cli)
+		for k, v := range tc.flags {
+			cmd.Flags().Set(k, v)
+		}
+		cmd.SetArgs(tc.args)
+		cmd.SetOutput(ioutil.Discard)
+		assert.ErrorContains(t, cmd.Execute(), tc.expectedError)
+	}
 }
