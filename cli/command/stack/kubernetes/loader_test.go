f768df9477ef29ed1e8cd6852a687072b34dd0b1
Silvin Lubecki <silvin.lubecki@docker.com>
2018-02-20 18:00:53 +0100

    Fix yaml marshaling
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>

--- cli/command/stack/kubernetes/loader_test.go
+++ cli/command/stack/kubernetes/loader_test.go
@@ -4,13 +4,11 @@ import (
 	"testing"
 
 	composetypes "github.com/docker/cli/cli/compose/types"
-	apiv1beta1 "github.com/docker/cli/kubernetes/compose/v1beta1"
 	"github.com/stretchr/testify/require"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestLoadStack(t *testing.T) {
-	s, err := LoadStack("foo", composetypes.Config{
+	s, err := loadStack("foo", composetypes.Config{
 		Version:  "3.1",
 		Filename: "banana",
 		Services: []composetypes.ServiceConfig{
@@ -25,12 +23,8 @@ func TestLoadStack(t *testing.T) {
 		},
 	})
 	require.NoError(t, err)
-	require.Equal(t, &apiv1beta1.Stack{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "foo",
-		},
-		Spec: apiv1beta1.StackSpec{
-			ComposeFile: string(`version: "3.1"
+	require.Equal(t, "foo", s.name)
+	require.Equal(t, string(`version: "3.1"
 services:
   bar:
     image: bar
@@ -40,7 +34,5 @@ networks: {}
 volumes: {}
 secrets: {}
 configs: {}
-`),
-		},
-	}, s)
+`), s.composeFile)
 }
