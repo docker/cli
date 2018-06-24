7d92898b2e27444915981cbd6fc8766d8542de23
Artur Juraszek <artur@juraszek.xyz>
2018-06-24 16:19:39 +0200

    Add `network` subcommand support to fish completions
    Signed-off-by: Artur Juraszek <artur@juraszek.xyz>

--- contrib/completion/fish/docker.fish
+++ contrib/completion/fish/docker.fish
@@ -16,7 +16,7 @@
 
 function __fish_docker_no_subcommand --description 'Test if docker has yet to be given the subcommand'
     for i in (commandline -opc)
-        if contains -- $i attach build commit config cp create diff events exec export history images import info inspect kill load login logout logs pause port ps pull push rename restart rm rmi run save search start stats stop tag top trust unpause version volume wait
+        if contains -- $i attach build commit config cp create diff events exec export history images import info inspect kill load login logout logs network pause port ps pull push rename restart rm rmi run save search start stats stop tag top trust unpause version volume wait
             return 1
         end
     end
