1bba6b45ee62f7fc52ac2c4370da36fbdfb7d378
Robert Zastrau <robert.zastrau@icloud.com>
2019-06-06 22:27:01 +0200

    REMOVE MULTIPLE CREDENTIAL ERASES
    Fix problem that no logout is performed, when dealing with the default
    registry. Also shortened the whole func to avoid appending to a second slice.
    This also saves a second for loop, because auth credentials can be removed
    as they are found. Not sure how the errors shall be handled in here.
    
    Signed-off-by: Robert Zastrau <robert.zastrau@icloud.com>

--- cli/command/registry/logout.go
+++ cli/command/registry/logout.go
@@ -31,24 +31,25 @@ func NewLogoutCommand(dockerCli command.Cli) *cobra.Command {
 
 func runLogout(dockerCli command.Cli, serverAddress string) error {
 	ctx := context.Background()
-	var isDefaultRegistry bool
-
-	if serverAddress == "" {
-		serverAddress = command.ElectAuthServer(ctx, dockerCli)
-		isDefaultRegistry = true
-	}
 
 	var (
-		loggedIn        bool
-		regsToLogout    []string
 		hostnameAddress = serverAddress
-		regsToTry       []string
+		loggedIn        bool     // is set later, when checking for credentials
+		regsToTry       []string // is set based on the type of registry
 	)
-	if !isDefaultRegistry {
+
+	// differentiate between default und private registry
+	if serverAddress == "" {
+		// if no server address given, handle case for default registry
+		serverAddress = command.ElectAuthServer(ctx, dockerCli)
+		regsToTry = []string{serverAddress}
+	} else {
+		// if server address given, handle case for private registry
 		hostnameAddress = registry.ConvertToHostname(serverAddress)
+
 		// the tries below are kept for backward compatibility where a user could have
 		// saved the registry in one of the following format.
-		regsToTry = append(regsToTry, hostnameAddress, "http://"+hostnameAddress, "https://"+hostnameAddress)
+		regsToTry = []string{hostnameAddress, "http://" + hostnameAddress, "https://" + hostnameAddress}
 	}
 
 	// check if we're logged in based on the records in the config file
@@ -56,20 +57,18 @@ func runLogout(dockerCli command.Cli, serverAddress string) error {
 	for _, s := range regsToTry {
 		if _, ok := dockerCli.ConfigFile().AuthConfigs[s]; ok {
 			loggedIn = true
-			regsToLogout = append(regsToLogout, s)
+
+			// remove credentials, for found auth config
+			fmt.Fprintf(dockerCli.Out(), "Removing login credentials for %s\n", hostnameAddress)
+			if err := dockerCli.ConfigFile().GetCredentialsStore(s).Erase(s); err != nil {
+				fmt.Fprintf(dockerCli.Err(), "WARNING: could not erase credentials: %v\n", err)
+			}
 		}
 	}
 
 	if !loggedIn {
 		fmt.Fprintf(dockerCli.Out(), "Not logged in to %s\n", hostnameAddress)
 		return nil
-	}
-
-	fmt.Fprintf(dockerCli.Out(), "Removing login credentials for %s\n", hostnameAddress)
-	for _, r := range regsToLogout {
-		if err := dockerCli.ConfigFile().GetCredentialsStore(r).Erase(r); err != nil {
-			fmt.Fprintf(dockerCli.Err(), "WARNING: could not erase credentials: %v\n", err)
-		}
 	}
 
 	return nil
