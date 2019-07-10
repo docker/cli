19a53d61ba95ca6bb97f781e9eacc7782b11d5b1
Sebastiaan van Stijn <github@gone.nl>
2019-07-10 14:22:01 +0200

    e2e: TestSigProxyWithTTY increase timeout
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- e2e/container/proxy_signal_test.go
+++ e2e/container/proxy_signal_test.go
@@ -34,14 +34,14 @@ func TestSigProxyWithTTY(t *testing.T) {
 	assert.NilError(t, err, "failed to start container")
 	defer icmd.RunCommand("docker", "container", "rm", "-f", containerName)
 
-	poll.WaitOn(t, containerExistsWithStatus(t, containerName, "running"), poll.WithDelay(100*time.Millisecond), poll.WithTimeout(5*time.Second))
+	poll.WaitOn(t, containerExistsWithStatus(t, containerName, "running"), poll.WithDelay(100*time.Millisecond), poll.WithTimeout(10*time.Second))
 
 	pid := cmd.Process.Pid
 	t.Logf("terminating PID %d", pid)
 	err = syscall.Kill(pid, syscall.SIGTERM)
 	assert.NilError(t, err)
 
-	poll.WaitOn(t, containerExistsWithStatus(t, containerName, "exited"), poll.WithDelay(100*time.Millisecond), poll.WithTimeout(5*time.Second))
+	poll.WaitOn(t, containerExistsWithStatus(t, containerName, "exited"), poll.WithDelay(100*time.Millisecond), poll.WithTimeout(10*time.Second))
 }
 
 func containerExistsWithStatus(t *testing.T, containerID, status string) func(poll.LogT) poll.Result {
