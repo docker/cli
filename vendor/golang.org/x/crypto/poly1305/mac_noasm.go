a727b80bfe6b03a6652984d2fade1290ac4e8e3f
ognyan nikolov <osn@kofar.io>
2020-02-07 16:12:59 +0200

    vendor: update golang.org/x/crypto 69ecbb4d6d5dab05e49161c6e77ea40a03…
    Includes golang/crypto@69ecbb4
    (forward-port of golang/crypto@8b5121b),
    which fixes CVE-2020-7919:
    
    - Panic in crypto/x509 certificate parsing and golang.org/x/crypto/cryptobyte
      On 32-bit architectures, a malformed input to crypto/x509 or the ASN.1 parsing
      functions of golang.org/x/crypto/cryptobyte can lead to a panic.
      The malformed certificate can be delivered via a crypto/tls connection to a
      client, or to a server that accepts client certificates. net/http clients can
      be made to crash by an HTTPS server, while net/http servers that accept client
      certificates will recover the panic and are unaffected.
      Thanks to Project Wycheproof for providing the test cases that led to the
      discovery of this issue. The issue is CVE-2020-7919 and Go issue golang.org/issue/36837.
    
    Signed-off-by: Ognyan Nikolov <onikolov@mirantis.com>

--- vendor/golang.org/x/crypto/poly1305/mac_noasm.go
+++ vendor/golang.org/x/crypto/poly1305/mac_noasm.go
@@ -2,12 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build s390x,!go1.11 !amd64,!s390x,!ppc64le gccgo appengine nacl
+// +build !amd64,!ppc64le gccgo appengine
 
 package poly1305
 
-func sum(out *[TagSize]byte, msg []byte, key *[32]byte) {
-	h := newMAC(key)
-	h.Write(msg)
-	h.Sum(out)
-}
+type mac struct{ macGeneric }
+
+func newMAC(key *[32]byte) mac { return mac{newMACGeneric(key)} }
