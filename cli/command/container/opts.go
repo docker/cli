3cf087c30077ddb17afee13d5915e4ae905c4665
fanjiyun <fan.jiyun@zte.com.cn>
2020-05-15 17:41:20 +0800

    control network interface order for containers
    Signed-off-by: fanjiyun <fan.jiyun@zte.com.cn>

--- cli/command/container/opts.go
+++ cli/command/container/opts.go
@@ -804,6 +804,7 @@ func parseNetworkAttachmentOpt(ep opts.NetworkAttachmentOpts) (*networktypes.End
 			LinkLocalIPs: ep.LinkLocalIPs,
 		}
 	}
+	epConfig.Priority = ep.Priority
 	return epConfig, nil
 }
 
