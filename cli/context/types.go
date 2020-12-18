1a21ffbb6382f3c3bc2a5afc93cb7421c8d3d05a
Guillaume Tardif <guillaume.tardif@gmail.com>
2020-12-18 17:05:32 +0100

    Find backend binary in hardcoded path, add explicit context create subcommands
    Signed-off-by: Guillaume Tardif <guillaume.tardif@gmail.com>

--- cli/context/types.go
+++ cli/context/types.go
@@ -3,27 +3,68 @@ package context
 import (
 	"fmt"
 	"os"
-	"syscall"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
 )
 
 const (
 	// ContextTypeECS is Amazon ECS context type
 	ContextTypeECS = "ecs"
-
 	// ContextTypeACI is MS Azure Container Instances context type
 	ContextTypeACI = "aci"
+
+	//TODO move in a separate file and have a windows version
+	backendFolder = "/usr/local/lib/docker/cli-backends"
 )
 
 // RunContextCLI replace the current process with dedicated CLI for this context type
 func RunContextCLI(context string) error {
-	executable, err := os.Executable()
-	if err != nil {
-		return err
+	if context != ContextTypeACI && context != ContextTypeECS {
+		return fmt.Errorf("unsupported context type: %q", context)
 	}
-	x := executable + "-" + context
+	x := filepath.Join(backendFolder, "compose-cli")
 	if _, err := os.Stat(x); os.IsNotExist(err) {
 		// TODO we could be more restrictive about supported types to prevent abuses using ContextType enum values
 		return fmt.Errorf("unsupported context type %q", context)
 	}
-	return syscall.Exec(x, os.Args[1:], os.Environ())
+	delegate(x) // will exit
+	return nil
+}
+
+func delegate(execBinary string) {
+	cmd := exec.Command(execBinary, os.Args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
+
+	signals := make(chan os.Signal, 1)
+	childExit := make(chan bool)
+	signal.Notify(signals) // catch all signals
+	go func() {
+		for {
+			select {
+			case sig := <-signals:
+				if cmd.Process == nil {
+					continue // can happen if receiving signal before the process is actually started
+				}
+				// nolint errcheck
+				cmd.Process.Signal(sig)
+			case <-childExit:
+				return
+			}
+		}
+	}()
+
+	err := cmd.Run()
+	childExit <- true
+	if err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			os.Exit(exiterr.ExitCode())
+		}
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
