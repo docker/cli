0ea587b0d73c47d4e6a9a22ac4f1b811e1fcf5fa
Conner Crosby <conner@cavcrosby.tech>
2022-04-08 22:43:28 -0400

    Add --force option to network rm subcommand
    The code is similar to that used by the volume rm subcommand, however,
    one difference I noticed was VolumeRemove takes the force flag/option
    was a parameter. This isn't the case for NetworkRemove.
    
    To get NetworkRemove to take a similar parameter, this would require
    modifying the Docker daemon. For now this isn't a route I wish to take
    when the code can be arrange to mimic the same behavior.
    
    Co-authored-by: Sebastiaan van Stijn <github@gone.nl>
    Signed-off-by: Conner Crosby <conner@cavcrosby.tech>

--- cli/command/network/client_test.go
+++ cli/command/network/client_test.go
@@ -13,6 +13,7 @@ type fakeClient struct {
 	networkCreateFunc     func(ctx context.Context, name string, options types.NetworkCreate) (types.NetworkCreateResponse, error)
 	networkConnectFunc    func(ctx context.Context, networkID, container string, config *network.EndpointSettings) error
 	networkDisconnectFunc func(ctx context.Context, networkID, container string, force bool) error
+	networkRemoveFunc     func(ctx context.Context, networkID string) error
 	networkListFunc       func(ctx context.Context, options types.NetworkListOptions) ([]types.NetworkResource, error)
 }
 
@@ -42,4 +43,15 @@ func (c *fakeClient) NetworkList(ctx context.Context, options types.NetworkListO
 		return c.networkListFunc(ctx, options)
 	}
 	return []types.NetworkResource{}, nil
+}
+
+func (c *fakeClient) NetworkRemove(ctx context.Context, networkID string) error {
+	if c.networkRemoveFunc != nil {
+		return c.networkRemoveFunc(ctx, networkID)
+	}
+	return nil
+}
+
+func (c *fakeClient) NetworkInspectWithRaw(ctx context.Context, network string, options types.NetworkInspectOptions) (types.NetworkResource, []byte, error) {
+	return types.NetworkResource{}, nil, nil
 }
