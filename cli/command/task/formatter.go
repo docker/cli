2c41bbc49fae55dcbec0f77d6efb438f3f064aeb
Sebastiaan van Stijn <github@gone.nl>
2022-11-28 16:22:54 +0100

    cli/command/task: taskContext.Error(): use ellipsis utility
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/task/formatter.go
+++ cli/command/task/formatter.go
@@ -123,11 +123,11 @@ func (c *taskContext) CurrentState() string {
 func (c *taskContext) Error() string {
 	// Trim and quote the error message.
 	taskErr := c.task.Status.Err
-	if c.trunc && len(taskErr) > maxErrLength {
-		taskErr = fmt.Sprintf("%s…", taskErr[:maxErrLength-1])
+	if c.trunc {
+		taskErr = formatter.Ellipsis(taskErr, maxErrLength)
 	}
 	if len(taskErr) > 0 {
-		taskErr = fmt.Sprintf("\"%s\"", taskErr)
+		taskErr = fmt.Sprintf(`"%s"`, taskErr)
 	}
 	return taskErr
 }
