cafdcf283e541fc51fcd504efab0754220d248e7
Sebastiaan van Stijn <github@gone.nl>
2023-05-09 22:17:27 +0200

    cli-plugins/manager: fix deprecation comment of Metadata.Experimental
    This field was marked deprecated in 977d3ae046ec6c64be8788a8712251ed547a2bdb,
    which is part of v20.10 and up, but the comment was missing a newline before
    the deprecation message, which may be picked up by IDEs, but is not matching
    the correct format, so may not be picked up by linters.
    
    This patch fixes the format, to make sure linters pick up that the field is
    deprecated.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 72e3813ab91d482c7a1f06abb16005cade7f002b)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli-plugins/manager/metadata.go
+++ cli-plugins/manager/metadata.go
@@ -23,6 +23,7 @@ type Metadata struct {
 	// URL is a pointer to the plugin's homepage.
 	URL string `json:",omitempty"`
 	// Experimental specifies whether the plugin is experimental.
+	//
 	// Deprecated: experimental features are now always enabled in the CLI
 	Experimental bool `json:",omitempty"`
 }
