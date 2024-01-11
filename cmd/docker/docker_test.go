86162f78166e2eb1142e981b20456d8a9ebe9309
Sebastiaan van Stijn <github@gone.nl>
2024-01-11 18:25:23 +0100

    feat: use main func ctx for cobra and use ctx in tests
    Explicitly create the context and set it on the CLI, instead of depending on
    NewDockerCli() to instance a default context.
    
    Co-authored-by: Sebastiaan van Stijn <github@gone.nl>
    Co-authored-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
    
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- cmd/docker/docker_test.go
+++ cmd/docker/docker_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"io"
 	"os"
 	"testing"
@@ -15,8 +16,10 @@ import (
 
 func TestClientDebugEnabled(t *testing.T) {
 	defer debug.Disable()
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
 
-	cli, err := command.NewDockerCli()
+	cli, err := command.NewDockerCli(command.WithBaseContext(ctx))
 	assert.NilError(t, err)
 	tcmd := newDockerCommand(cli)
 	tcmd.SetFlag("debug", "true")
@@ -39,7 +42,13 @@ func runCliCommand(t *testing.T, r io.ReadCloser, w io.Writer, args ...string) e
 	if w == nil {
 		w = io.Discard
 	}
-	cli, err := command.NewDockerCli(command.WithInputStream(r), command.WithCombinedStreams(w))
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
+
+	cli, err := command.NewDockerCli(
+		command.WithBaseContext(ctx),
+		command.WithInputStream(r),
+		command.WithCombinedStreams(w))
 	assert.NilError(t, err)
 	tcmd := newDockerCommand(cli)
 
