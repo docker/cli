55a1f6eb73f958d3f915f8eafd0d056e428919d3
Sebastiaan van Stijn <github@gone.nl>
2024-06-19 14:02:40 +0200

    cli-plugins/manager: add GoDoc for getPluginDirs, defaultSystemPluginDirs
    Add some documentation about their purpose, and document order of preference
    when resolving plugins.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli-plugins/manager/manager_windows.go
+++ cli-plugins/manager/manager_windows.go
@@ -5,6 +5,16 @@ import (
 	"path/filepath"
 )
 
+// defaultSystemPluginDirs are the platform-specific locations to search
+// for plugins in order of preference.
+//
+// Plugin-discovery is performed in the following order of preference:
+//
+// 1. The "cli-plugins" directory inside the CLIs config-directory (usually "~/.docker/cli-plugins").
+// 2. Additional plugin directories as configured through [ConfigFile.CLIPluginsExtraDirs].
+// 3. Platform-specific defaultSystemPluginDirs (as defined below).
+//
+// [ConfigFile.CLIPluginsExtraDirs]: https://pkg.go.dev/github.com/docker/cli@v26.1.4+incompatible/cli/config/configfile#ConfigFile.CLIPluginsExtraDirs
 var defaultSystemPluginDirs = []string{
 	filepath.Join(os.Getenv("ProgramData"), "Docker", "cli-plugins"),
 	filepath.Join(os.Getenv("ProgramFiles"), "Docker", "cli-plugins"),
