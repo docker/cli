c4099e1d51f652e38eaab208a2d2518caa9d6ead
Ehsan Salamati <ehsan.salamati@instacart.com>
2024-08-01 16:45:10 -0400

    Fixes duplicate entries in docker node ps when multi nodes are passed. For example, 'docker node ps node1 node2'.
    Signed-off-by: Ehsan Salamati <e.salamati.taba@gmail.com>

--- cli/command/node/ps.go
+++ cli/command/node/ps.go
@@ -2,6 +2,7 @@ package node
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/docker/cli/cli"
@@ -74,9 +75,9 @@ func runPs(ctx context.Context, dockerCli command.Cli, options psOptions) error 
 			continue
 		}
 
-		filter := options.filter.Value()
+		filter := options.filter.Value().Clone()
 		filter.Add("node", node.ID)
-
+		fmt.Println("filter", filter)
 		nodeTasks, err := client.TaskList(ctx, types.TaskListOptions{Filters: filter})
 		if err != nil {
 			errs = append(errs, err.Error())
