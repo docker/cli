0bac5a707b23b588a379ca1a05a0bf5a77155c32
Laura Brehm <laurabrehm@hey.com>
2024-08-05 12:16:02 +0100

    filestore: don't print warning multiple times
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- cli/config/credentials/file_store.go
+++ cli/config/credentials/file_store.go
@@ -65,6 +65,8 @@ Configure a credential helper to remove this warning. See
 https://docs.docker.com/go/credential-store/
 `
 
+var alreadyPrinted bool
+
 // Store saves the given credentials in the file store.
 func (c *fileStore) Store(authConfig types.AuthConfig) error {
 	authConfigs := c.file.GetAuthConfigs()
@@ -73,11 +75,12 @@ func (c *fileStore) Store(authConfig types.AuthConfig) error {
 		return err
 	}
 
-	if authConfig.Password != "" {
+	if !alreadyPrinted && authConfig.Password != "" {
 		// Display a warning if we're storing the users password (not a token).
 		//
 		// FIXME(thaJeztah): make output configurable instead of hardcoding to os.Stderr
 		_, _ = fmt.Fprintln(os.Stderr, fmt.Sprintf(unencryptedWarning, c.file.GetFilename()))
+		alreadyPrinted = true
 	}
 
 	return nil
