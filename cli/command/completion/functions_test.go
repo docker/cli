8f2e5662e7e4d74f5cc33887749f9378287df627
Sebastiaan van Stijn <github@gone.nl>
2024-10-13 17:48:49 +0200

    completion: add test for EnvVarNames
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/completion/functions_test.go
+++ cli/command/completion/functions_test.go
@@ -1,12 +1,27 @@
 package completion
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/spf13/cobra"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
+	"gotest.tools/v3/env"
 )
+
+func TestCompleteEnvVarNames(t *testing.T) {
+	env.PatchAll(t, map[string]string{
+		"ENV_A": "hello-a",
+		"ENV_B": "hello-b",
+	})
+	values, directives := EnvVarNames(nil, nil, "")
+	assert.Check(t, is.Equal(directives&cobra.ShellCompDirectiveNoFileComp, cobra.ShellCompDirectiveNoFileComp), "Should not perform file completion")
+
+	sort.Strings(values)
+	expected := []string{"ENV_A", "ENV_B"}
+	assert.Check(t, is.DeepEqual(values, expected))
+}
 
 func TestCompletePlatforms(t *testing.T) {
 	values, directives := Platforms(nil, nil, "")
