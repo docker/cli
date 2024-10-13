be197da6b848c4f3efbaf6bcdde5f50868acbb6c
Sebastiaan van Stijn <github@gone.nl>
2024-10-13 17:54:40 +0200

    completion: add test for NoComplete
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/completion/functions_test.go
+++ cli/command/completion/functions_test.go
@@ -37,6 +37,12 @@ func TestCompleteFromList(t *testing.T) {
 	assert.Check(t, is.DeepEqual(values, expected))
 }
 
+func TestCompleteNoComplete(t *testing.T) {
+	values, directives := NoComplete(nil, nil, "")
+	assert.Check(t, is.Equal(directives, cobra.ShellCompDirectiveNoFileComp))
+	assert.Check(t, is.Len(values, 0))
+}
+
 func TestCompletePlatforms(t *testing.T) {
 	values, directives := Platforms(nil, nil, "")
 	assert.Check(t, is.Equal(directives&cobra.ShellCompDirectiveNoFileComp, cobra.ShellCompDirectiveNoFileComp), "Should not perform file completion")
