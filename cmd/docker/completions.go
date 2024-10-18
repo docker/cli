670f81803feb1252caf58d6477f9515f978ea8c9
Sebastiaan van Stijn <github@gone.nl>
2024-10-18 11:20:38 +0200

    cmd/docker: add tests for flag-completions, and refactor
    Remove the registerCompletionFuncForGlobalFlags for now, as
    the error it returned was ignored, so it didn't add much
    benefit, other than abstracting things.
    
    Split the underlying completion-functions to separate
    functions, and add some basic tests for them.
    
    Remove the completions helper, as it now didn't add much,
    and it saved having the dependency on the package.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cmd/docker/completions.go
+++ cmd/docker/completions.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/docker/cli/cli/command/completion"
 	"github.com/docker/cli/cli/context/store"
 	"github.com/spf13/cobra"
 )
@@ -10,18 +9,15 @@ type contextStoreProvider interface {
 	ContextStore() store.Store
 }
 
-func registerCompletionFuncForGlobalFlags(dockerCLI contextStoreProvider, cmd *cobra.Command) error {
-	err := cmd.RegisterFlagCompletionFunc("context", func(*cobra.Command, []string, string) ([]string, cobra.ShellCompDirective) {
+func completeContextNames(dockerCLI contextStoreProvider) func(*cobra.Command, []string, string) ([]string, cobra.ShellCompDirective) {
+	return func(*cobra.Command, []string, string) ([]string, cobra.ShellCompDirective) {
 		names, _ := store.Names(dockerCLI.ContextStore())
 		return names, cobra.ShellCompDirectiveNoFileComp
-	})
-	if err != nil {
-		return err
 	}
-	err = cmd.RegisterFlagCompletionFunc("log-level", completion.FromList("debug", "info", "warn", "error", "fatal"))
-	if err != nil {
-		return err
-	}
+}
 
-	return nil
+var logLevels = []string{"debug", "info", "warn", "error", "fatal", "panic"}
+
+func completeLogLevels(*cobra.Command, []string, string) ([]string, cobra.ShellCompDirective) {
+	return cobra.FixedCompletions(logLevels, cobra.ShellCompDirectiveNoFileComp)(nil, nil, "")
 }
