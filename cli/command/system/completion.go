c5306f3c1ab4f5020ce7ee213c615802447441eb
Harald Albers <github@albersweb.de>
2024-10-18 14:28:30 +0000

    Use existing constants in completion of types and events in `events --filter`
    Signed-off-by: Harald Albers <github@albersweb.de>

--- cli/command/system/completion.go
+++ cli/command/system/completion.go
@@ -3,56 +3,82 @@ package system
 import (
 	"strings"
 
+	"github.com/docker/docker/api/types/events"
+
 	"github.com/docker/cli/cli/command/completion"
 	"github.com/spf13/cobra"
 )
 
 var (
 	eventFilters = []string{"container", "daemon", "event", "image", "label", "network", "node", "scope", "type", "volume"}
-	eventNames   = []string{
-		"attach",
-		"commit",
-		"connect",
-		"copy",
-		"create",
-		"delete",
-		"destroy",
-		"detach",
-		"die",
-		"disable",
-		"disconnect",
-		"enable",
-		"exec_create",
-		"exec_detach",
-		"exec_die",
-		"exec_start",
-		"export",
-		"health_status",
-		"import",
-		"install",
-		"kill",
-		"load",
-		"mount",
-		"oom",
-		"pause",
-		"pull",
-		"push",
-		"reload",
-		"remove",
-		"rename",
-		"resize",
-		"restart",
-		"save",
-		"start",
-		"stop",
-		"tag",
-		"top",
-		"unmount",
-		"unpause",
-		"untag",
-		"update",
+
+	// eventTypes is a list of all event types.
+	// This should be moved to the moby codebase once its usage is consolidated here.
+	eventTypes = []events.Type{
+		events.BuilderEventType,
+		events.ConfigEventType,
+		events.ContainerEventType,
+		events.DaemonEventType,
+		events.ImageEventType,
+		events.NetworkEventType,
+		events.NodeEventType,
+		events.PluginEventType,
+		events.SecretEventType,
+		events.ServiceEventType,
+		events.VolumeEventType,
 	}
-	eventTypes = []string{"config", "container", "daemon", "image", "network", "node", "plugin", "secret", "service", "volume"}
+
+	// eventActions is a list of all event actions.
+	// This should be moved to the moby codebase once its usage is consolidated here.
+	eventActions = []events.Action{
+		events.ActionCreate,
+		events.ActionStart,
+		events.ActionRestart,
+		events.ActionStop,
+		events.ActionCheckpoint,
+		events.ActionPause,
+		events.ActionUnPause,
+		events.ActionAttach,
+		events.ActionDetach,
+		events.ActionResize,
+		events.ActionUpdate,
+		events.ActionRename,
+		events.ActionKill,
+		events.ActionDie,
+		events.ActionOOM,
+		events.ActionDestroy,
+		events.ActionRemove,
+		events.ActionCommit,
+		events.ActionTop,
+		events.ActionCopy,
+		events.ActionArchivePath,
+		events.ActionExtractToDir,
+		events.ActionExport,
+		events.ActionImport,
+		events.ActionSave,
+		events.ActionLoad,
+		events.ActionTag,
+		events.ActionUnTag,
+		events.ActionPush,
+		events.ActionPull,
+		events.ActionPrune,
+		events.ActionDelete,
+		events.ActionEnable,
+		events.ActionDisable,
+		events.ActionConnect,
+		events.ActionDisconnect,
+		events.ActionReload,
+		events.ActionMount,
+		events.ActionUnmount,
+		events.ActionExecCreate,
+		events.ActionExecStart,
+		events.ActionExecDie,
+		events.ActionExecDetach,
+		events.ActionHealthStatus,
+		events.ActionHealthStatusRunning,
+		events.ActionHealthStatusHealthy,
+		events.ActionHealthStatusUnhealthy,
+	}
 )
 
 func completeFilters(dockerCLI completion.APIClientProvider) completion.ValidArgsFn {
@@ -65,7 +91,7 @@ func completeFilters(dockerCLI completion.APIClientProvider) completion.ValidArg
 			return prefixWith("container=", names), cobra.ShellCompDirectiveDefault
 		}
 		if strings.HasPrefix(toComplete, "event=") {
-			return prefixWith("event=", eventNames), cobra.ShellCompDirectiveDefault
+			return prefixWith("event=", validEventNames()), cobra.ShellCompDirectiveDefault
 		}
 		if strings.HasPrefix(toComplete, "label=") {
 			return nil, cobra.ShellCompDirectiveNoFileComp
@@ -78,7 +104,7 @@ func completeFilters(dockerCLI completion.APIClientProvider) completion.ValidArg
 			return prefixWith("network=", names), cobra.ShellCompDirectiveDefault
 		}
 		if strings.HasPrefix(toComplete, "type=") {
-			return prefixWith("type=", eventTypes), cobra.ShellCompDirectiveDefault
+			return prefixWith("type=", eventTypeNames()), cobra.ShellCompDirectiveDefault
 		}
 		return postfixWith("=", eventFilters), cobra.ShellCompDirectiveNoSpace
 	}
@@ -98,4 +124,28 @@ func postfixWith(postfix string, values []string) []string {
 		result[i] = v + postfix
 	}
 	return result
+}
+
+// eventTypeNames provides a list of all event types.
+// The list is derived from eventTypes.
+func eventTypeNames() []string {
+	names := make([]string, len(eventTypes))
+	for i, eventType := range eventTypes {
+		names[i] = string(eventType)
+	}
+	return names
+}
+
+// validEventNames provides a list of all event actions.
+// The list is derived from eventActions.
+// Actions that are not suitable for usage in completions are removed.
+func validEventNames() []string {
+	names := []string{}
+	for _, eventAction := range eventActions {
+		if strings.Contains(string(eventAction), " ") {
+			continue
+		}
+		names = append(names, string(eventAction))
+	}
+	return names
 }
