1377310110548af0639ddc1096949fb95d4ed8e4
Sebastiaan van Stijn <github@gone.nl>
2024-10-19 12:10:46 +0200

    cli/command: PromptUserForCredentials: always trim password
    we don't support empty passwords; when prompting the user for a password,
    we already trim the result, but we didn't do the same for a password that's
    passed through stdin or through the `-p` / `--password` flag.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit a21a5f42433267052441cdb7ebe7604dfcc7f159)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/registry.go
+++ cli/command/registry.go
@@ -157,6 +157,7 @@ func PromptUserForCredentials(ctx context.Context, cli Cli, argUser, argPassword
 		}
 	}
 
+	argPassword = strings.TrimSpace(argPassword)
 	if argPassword == "" {
 		restoreInput, err := DisableInputEcho(cli.In())
 		if err != nil {
