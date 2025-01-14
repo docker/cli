9abd0ebdf4394703aa430fd6d7cb0875b4c81d62
Sebastiaan van Stijn <github@gone.nl>
2025-01-14 19:36:48 +0100

    cli/command: update semconv to 1.26.0
    Align the version with what's used by buildkit.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/telemetry.go
+++ cli/command/telemetry.go
@@ -14,7 +14,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
