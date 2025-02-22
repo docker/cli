d2b15b5d0cc6ee48590d394e5045cbf8d14818b8
Quang Hieu Le <hanle.cs23@gmail.com>
2025-02-21 23:00:56 -0500

    Verify container opts.go for G115 lint warning
    Currently the warning is there for conversion from int64 to uint64, regarding MemBytes flag (128M, 2G). While int64 allows negative value, the actual value that is passing for this flag cannot be negative due to the concept of negative memory does not exist (as far as the author know). At the same time, int64 and uint64 has the same byte size, so this conversion won't cause overflow.
    
    Signed-off-by: Quang Hieu Le <hanle.cs23@gmail.com>

--- cli/command/container/opts.go
+++ cli/command/container/opts.go
@@ -620,7 +620,7 @@ func parse(flags *pflag.FlagSet, copts *containerOptions, serverOS string) (*con
 		BlkioDeviceReadIOps:  copts.deviceReadIOps.GetList(),
 		BlkioDeviceWriteIOps: copts.deviceWriteIOps.GetList(),
 		IOMaximumIOps:        copts.ioMaxIOps,
-		IOMaximumBandwidth:   uint64(copts.ioMaxBandwidth),
+		IOMaximumBandwidth:   uint64(copts.ioMaxBandwidth), //nolint:gosec
 		Ulimits:              copts.ulimits.GetList(),
 		DeviceCgroupRules:    copts.deviceCgroupRules.GetAll(),
 		Devices:              deviceMappings,
