08f86507b42642d9782ddf7abf8d1f1d1507254b
Sebastiaan van Stijn <github@gone.nl>
2025-03-01 02:33:48 +0100

    cli/command: remove deprecated ManifestStore from CLI interface
    This method is a shallow wrapper around manifeststore.NewStore, but
    due to its signature resulted in various dependencies becoming a dependency
    of the "command" package. Consequence of this was that cli-plugins, which
    need the cli/command package, would also get those dependencies. It is no
    longer used, and was deprecated in e32d5d56f5fea7cdfb91a317d517cd163c4954d7.
    
    This patch removes the ManifestStore method from the interface
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/cli_deprecated.go
+++ cli/command/cli_deprecated.go
@@ -2,32 +2,17 @@ package command
 
 import (
 	"context"
-	"path/filepath"
 
-	"github.com/docker/cli/cli/config"
-	manifeststore "github.com/docker/cli/cli/manifest/store"
 	registryclient "github.com/docker/cli/cli/registry/client"
 	"github.com/docker/docker/api/types/registry"
 )
 
 type DeprecatedManifestClient interface {
-	// ManifestStore returns a store for local manifests
-	//
-	// Deprecated: use [manifeststore.NewStore] instead. This method is no longer used and will be removed in the next release.
-	ManifestStore() manifeststore.Store
-
 	// RegistryClient returns a client for communicating with a Docker distribution
 	// registry.
 	//
 	// Deprecated: use [registryclient.NewRegistryClient]. This method is no longer used and will be removed in the next release.
 	RegistryClient(bool) registryclient.RegistryClient
-}
-
-// ManifestStore returns a store for local manifests
-//
-// Deprecated: use [manifeststore.NewStore] instead. This method is no longer used and will be removed in the next release.
-func (*DockerCli) ManifestStore() manifeststore.Store {
-	return manifeststore.NewStore(filepath.Join(config.Dir(), "manifests"))
 }
 
 // RegistryClient returns a client for communicating with a Docker distribution
