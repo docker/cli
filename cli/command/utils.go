ce3090ccc476f47a132295336c2060dd8ed26f66
Sebastiaan van Stijn <github@gone.nl>
2025-03-09 22:20:12 +0100

    cli/command: move PrettyPrint utility to cli/command/formatter
    This utility was only used internally, and has no external consumers;
    move it to the "formatter" package, which is also imported in all files
    using this utility.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/utils.go
+++ cli/command/utils.go
@@ -51,30 +51,6 @@ func CopyToFile(outfile string, r io.Reader) error {
 	return nil
 }
 
-// capitalizeFirst capitalizes the first character of string
-func capitalizeFirst(s string) string {
-	switch l := len(s); l {
-	case 0:
-		return s
-	case 1:
-		return strings.ToLower(s)
-	default:
-		return strings.ToUpper(string(s[0])) + strings.ToLower(s[1:])
-	}
-}
-
-// PrettyPrint outputs arbitrary data for human formatted output by uppercasing the first letter.
-func PrettyPrint(i any) string {
-	switch t := i.(type) {
-	case nil:
-		return "None"
-	case string:
-		return capitalizeFirst(t)
-	default:
-		return capitalizeFirst(fmt.Sprintf("%s", t))
-	}
-}
-
 var ErrPromptTerminated = errdefs.Cancelled(errors.New("prompt terminated"))
 
 // DisableInputEcho disables input echo on the provided streams.In.
