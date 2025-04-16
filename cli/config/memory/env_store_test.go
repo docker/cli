e0a4face0b766851fc8d5b1cb190e971e9bd630b
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2025-04-16 16:40:19 +0200

    Create in-memory store
    Signed-off-by: Alano Terblanche <18033717+Benehiko@users.noreply.github.com>

--- cli/config/memory/env_store_test.go
+++ cli/config/memory/env_store_test.go
@@ -1,7 +1,6 @@
-package credentials
+package memory
 
 import (
-	"encoding/json"
 	"testing"
 
 	"github.com/docker/cli/cli/config/types"
@@ -9,21 +8,15 @@ import (
 )
 
 func TestEnvStore(t *testing.T) {
-	envConfig := map[string]types.AuthConfig{
+	config := map[string]types.AuthConfig{
 		"https://example.com": {
 			Email:         "something-something",
 			ServerAddress: "https://example.com",
 			Auth:          "super_secret_token",
 		},
 	}
-	d, err := json.Marshal(map[string]map[string]types.AuthConfig{
-		"auth": envConfig,
-	})
-	assert.NilError(t, err)
 
-	t.Setenv("DOCKER_AUTH_CONFIG", string(d))
-
-	fileConfig := map[string]types.AuthConfig{
+	fallbackConfig := map[string]types.AuthConfig{
 		"https://only-in-file.com": {
 			Email:         "something-something",
 			ServerAddress: "https://only-in-file.com",
@@ -31,44 +24,35 @@ func TestEnvStore(t *testing.T) {
 		},
 	}
 
-	var saveCount int
-	fileStore := NewFileStore(&fakeStore{
-		configs: fileConfig,
-		saveFn: func(*fakeStore) error {
-			saveCount++
-			return nil
-		},
-	})
+	fallbackStore := NewInMemoryStore(fallbackConfig)
 
-	envStore := NewEnvStore(fileStore)
+	memoryStore := NewInMemoryStore(config, WithFallbackStore(fallbackStore))
 
 	t.Run("case=get credentials from env", func(t *testing.T) {
-		c, err := envStore.Get("https://example.com")
+		c, err := memoryStore.Get("https://example.com")
 		assert.NilError(t, err)
-		assert.Equal(t, c, envConfig["https://example.com"])
+		assert.Equal(t, c, config["https://example.com"])
 	})
 
 	t.Run("case=get credentials from file", func(t *testing.T) {
-		c, err := envStore.Get("https://only-in-file.com")
+		c, err := memoryStore.Get("https://only-in-file.com")
 		assert.NilError(t, err)
-		assert.Equal(t, c, fileConfig["https://only-in-file.com"])
+		assert.Equal(t, c, fallbackConfig["https://only-in-file.com"])
 	})
 
 	t.Run("case=storing credentials should not update env", func(t *testing.T) {
-		err := envStore.Store(types.AuthConfig{
+		err := memoryStore.Store(types.AuthConfig{
 			Email:         "not-in-env",
 			ServerAddress: "https://not-in-env",
 			Auth:          "not-in-env",
 		})
 		assert.NilError(t, err)
-		assert.Equal(t, saveCount, 1)
 	})
 
 	t.Run("case=delete credentials should not update env", func(t *testing.T) {
-		err := envStore.Erase("https://example.com")
+		err := memoryStore.Erase("https://example.com")
 		assert.NilError(t, err)
-		c, err := envStore.Get("https://example.com")
-		assert.NilError(t, err)
-		assert.Equal(t, c, envConfig["https://example.com"])
+		_, err = memoryStore.Get("https://example.com")
+		assert.Equal(t, IsErrValueNotFound(err), true)
 	})
 }
