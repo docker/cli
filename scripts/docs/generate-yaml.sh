537acbe98b4467f78873bce20a5cb9694f3e0f80
Sebastiaan van Stijn <github@gone.nl>
2025-04-25 18:27:11 +0200

    scripts/with-go-mod.sh: use symlink instead of -modfile
    While the "-modfile=vendor.mod" is slightly more correct, using a
    symlink allows for most of the go tools to work "as usual", just
    without an acual `go.mod` being committed in the repository.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- scripts/docs/generate-yaml.sh
+++ scripts/docs/generate-yaml.sh
@@ -4,4 +4,4 @@ set -eu
 
 mkdir -p docs/yaml
 set -x
-go run -mod=vendor -modfile=vendor.mod -tags docsgen ./docs/generate/generate.go --formats yaml --source "./docs/reference/commandline" --target "./docs/yaml"
+go run -mod=vendor -tags docsgen ./docs/generate/generate.go --formats yaml --source "./docs/reference/commandline" --target "./docs/yaml"
