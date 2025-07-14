53d02ece89349a54e41c24d890cba85d09323fd2
Sebastiaan van Stijn <github@gone.nl>
2025-07-14 22:30:30 +0200

    remove use of github.com/docker/docker/pkg/longpath
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/image/build/context_windows.go
+++ cli/command/image/build/context_windows.go
@@ -2,8 +2,7 @@ package build
 
 import (
 	"path/filepath"
-
-	"github.com/docker/docker/pkg/longpath"
+	"strings"
 )
 
 func getContextRoot(srcPath string) (string, error) {
@@ -11,5 +10,27 @@ func getContextRoot(srcPath string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return longpath.AddPrefix(cr), nil
+	return addPrefix(cr), nil
+}
+
+// longPathPrefix is the longpath prefix for Windows file paths.
+const longPathPrefix = `\\?\`
+
+// addPrefix adds the Windows long path prefix to the path provided if
+// it does not already have it.
+//
+// See https://github.com/moby/moby/pull/15898
+//
+// This is a copy of [longpath.AddPrefix].
+//
+// [longpath.AddPrefix]:https://pkg.go.dev/github.com/docker/docker@v28.3.2+incompatible/pkg/longpath#AddPrefix
+func addPrefix(path string) string {
+	if strings.HasPrefix(path, longPathPrefix) {
+		return path
+	}
+	if strings.HasPrefix(path, `\\`) {
+		// This is a UNC path, so we need to add 'UNC' to the path as well.
+		return longPathPrefix + `UNC` + path[1:]
+	}
+	return longPathPrefix + path
 }
