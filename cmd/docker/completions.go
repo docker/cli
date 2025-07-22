252c11f084a5e20af0a90ff9f992d46e91d04052
keitosuwahara <keitosuwahara0816@gmail.com>
2025-07-22 17:44:33 +0900

    Improved error handling (completeContextNames function)
    Signed-off-by: keitosuwahara <keitosuwahara0816@gmail.com>

--- cmd/docker/completions.go
+++ cmd/docker/completions.go
@@ -11,7 +11,11 @@ type contextStoreProvider interface {
 
 func completeContextNames(dockerCLI contextStoreProvider) func(*cobra.Command, []string, string) ([]string, cobra.ShellCompDirective) {
 	return func(*cobra.Command, []string, string) ([]string, cobra.ShellCompDirective) {
-		names, _ := store.Names(dockerCLI.ContextStore())
+		names, err := store.Names(dockerCLI.ContextStore())
+		if err != nil {
+			cobra.CompErrorln("failed to get context names: " + err.Error())
+			return nil, cobra.ShellCompDirectiveError
+		}
 		return names, cobra.ShellCompDirectiveNoFileComp
 	}
 }
