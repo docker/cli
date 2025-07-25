ca2f4d5b4fbfef04ea891fd7646688da6eb958d8
Sebastiaan van Stijn <github@gone.nl>
2025-07-25 23:39:17 +0200

    cli/command/plugin: remove DCT
    Plugins are not widely used, and there's no known plugins that use
    content-trust. We're working on updating the authentication stack
    in the CLI, and the trust implementation hinders us in making
    changes, so removing parts that are not high-priority (ahead of
    full deprecation of DCT).
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/plugin/upgrade.go
+++ cli/command/plugin/upgrade.go
@@ -31,7 +31,9 @@ func newUpgradeCommand(dockerCli command.Cli) *cobra.Command {
 	}
 
 	flags := cmd.Flags()
-	loadPullFlags(dockerCli, &options, flags)
+	flags.BoolVar(&options.grantPerms, "grant-all-permissions", false, "Grant all permissions necessary to run the plugin")
+	flags.Bool("disable-content-trust", dockerCli.ContentTrustEnabled(), "Skip image verification (deprecated)")
+	_ = flags.MarkHidden("disable-content-trust")
 	flags.BoolVar(&options.skipRemoteCheck, "skip-remote-check", false, "Do not check if specified remote plugin matches existing plugin image")
 	return cmd
 }
