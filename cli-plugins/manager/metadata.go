099820d8cafd8b1db627683552dcb3762c92e609
Sebastiaan van Stijn <github@gone.nl>
2025-08-04 11:25:01 +0200

    cli-plugins/manager: deprecate metadata aliases
    These aliases were added in 4321293972a4ed804e8c063868cc5da6147ce73b
    (part of v28.0), but did not deprecate them. They are no longer used
    in the CLI itself, but may be used by cli-plugin implementations.
    
    This deprecates the aliases in `cli-plugins/manager` in favor of
    their equivalent in `cli-plugins/manager/metadata`:
    
    - `NamePrefix`
    - `MetadataSubcommandName`
    - `HookSubcommandName`
    - `Metadata`
    - `ReexecEnvvar`
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 5876b2941c8b0cbefade16d6f857f1899a43f51b)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli-plugins/manager/metadata.go
+++ cli-plugins/manager/metadata.go
@@ -6,18 +6,26 @@ import (
 
 const (
 	// NamePrefix is the prefix required on all plugin binary names
+	//
+	// Deprecated: use [metadata.NamePrefix]. This alias will be removed in a future release.
 	NamePrefix = metadata.NamePrefix
 
 	// MetadataSubcommandName is the name of the plugin subcommand
 	// which must be supported by every plugin and returns the
 	// plugin metadata.
+	//
+	// Deprecated: use [metadata.MetadataSubcommandName]. This alias will be removed in a future release.
 	MetadataSubcommandName = metadata.MetadataSubcommandName
 
 	// HookSubcommandName is the name of the plugin subcommand
 	// which must be implemented by plugins declaring support
 	// for hooks in their metadata.
+	//
+	// Deprecated: use [metadata.HookSubcommandName]. This alias will be removed in a future release.
 	HookSubcommandName = metadata.HookSubcommandName
 )
 
 // Metadata provided by the plugin.
+//
+// Deprecated: use [metadata.Metadata]. This alias will be removed in a future release.
 type Metadata = metadata.Metadata
