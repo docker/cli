98b82d515651d61301fd78abeb227bddc58696a2
Sebastiaan van Stijn <github@gone.nl>
2025-08-18 21:50:53 +0200

    cli-plugins/manager: deprecate annotation metadata aliases
    These aliases were added in 292713c887b17339106e9f7f3647ed50bebc675d
    (part of v28.0), but did not deprecate them. They are no longer used
    in the CLI itself, but may be used by cli-plugin implementations.
    
    This deprecates the aliases in `cli-plugins/manager` in favor of
    their equivalent in `cli-plugins/manager/metadata`:
    
    - `CommandAnnotationPlugin`
    - `CommandAnnotationPluginVendor`
    - `CommandAnnotationPluginVersion`
    - `CommandAnnotationPluginInvalid`
    - `CommandAnnotationPluginCommandPath`
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 72f76f2720e5a31918f991ee797bfc3041fad37c)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli-plugins/manager/annotations.go
+++ cli-plugins/manager/annotations.go
@@ -6,25 +6,35 @@ const (
 	// CommandAnnotationPlugin is added to every stub command added by
 	// AddPluginCommandStubs with the value "true" and so can be
 	// used to distinguish plugin stubs from regular commands.
+	//
+	// Deprecated: use [metadata.CommandAnnotationPlugin]. This alias will be removed in the next release.
 	CommandAnnotationPlugin = metadata.CommandAnnotationPlugin
 
 	// CommandAnnotationPluginVendor is added to every stub command
 	// added by AddPluginCommandStubs and contains the vendor of
 	// that plugin.
+	//
+	// Deprecated: use [metadata.CommandAnnotationPluginVendor]. This alias will be removed in the next release.
 	CommandAnnotationPluginVendor = metadata.CommandAnnotationPluginVendor
 
 	// CommandAnnotationPluginVersion is added to every stub command
 	// added by AddPluginCommandStubs and contains the version of
 	// that plugin.
+	//
+	// Deprecated: use [metadata.CommandAnnotationPluginVersion]. This alias will be removed in the next release.
 	CommandAnnotationPluginVersion = metadata.CommandAnnotationPluginVersion
 
 	// CommandAnnotationPluginInvalid is added to any stub command
 	// added by AddPluginCommandStubs for an invalid command (that
 	// is, one which failed it's candidate test) and contains the
 	// reason for the failure.
+	//
+	// Deprecated: use [metadata.CommandAnnotationPluginInvalid]. This alias will be removed in the next release.
 	CommandAnnotationPluginInvalid = metadata.CommandAnnotationPluginInvalid
 
 	// CommandAnnotationPluginCommandPath is added to overwrite the
 	// command path for a plugin invocation.
+	//
+	// Deprecated: use [metadata.CommandAnnotationPluginCommandPath]. This alias will be removed in the next release.
 	CommandAnnotationPluginCommandPath = metadata.CommandAnnotationPluginCommandPath
 )
