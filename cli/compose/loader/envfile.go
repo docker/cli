e650803f091a1e6fc30c5d71b2aae743c22a73db
Sebastiaan van Stijn <github@gone.nl>
2025-08-20 14:55:28 +0200

    opts: deprecate ParseEnvFile
    It was a wrapper around kvfile.Load, which should be used instead.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/compose/loader/envfile.go
+++ cli/compose/loader/envfile.go
@@ -1,4 +1,4 @@
-package opts
+package loader
 
 import (
 	"os"
@@ -6,19 +6,21 @@ import (
 	"github.com/docker/cli/pkg/kvfile"
 )
 
-// ParseEnvFile reads a file with environment variables enumerated by lines
+// parseEnvFile reads a file with environment variables enumerated by lines
 //
 // “Environment variable names used by the utilities in the Shell and
-// Utilities volume of IEEE Std 1003.1-2001 consist solely of uppercase
+// Utilities volume of [IEEE Std 1003.1-2001] consist solely of uppercase
 // letters, digits, and the '_' (underscore) from the characters defined in
 // Portable Character Set and do not begin with a digit. *But*, other
 // characters may be permitted by an implementation; applications shall
 // tolerate the presence of such names.”
-// -- http://pubs.opengroup.org/onlinepubs/009695399/basedefs/xbd_chap08.html
 //
-// As of #16585, it's up to application inside docker to validate or not
+// As of [moby-16585], it's up to application inside docker to validate or not
 // environment variables, that's why we just strip leading whitespace and
 // nothing more.
-func ParseEnvFile(filename string) ([]string, error) {
+//
+// [IEEE Std 1003.1-2001]: http://pubs.opengroup.org/onlinepubs/009695399/basedefs/xbd_chap08.html
+// [moby-16585]: https://github.com/moby/moby/issues/16585
+func parseEnvFile(filename string) ([]string, error) {
 	return kvfile.Parse(filename, os.LookupEnv)
 }
