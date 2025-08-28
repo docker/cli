c3be589c1638073b475acbcd69c26fa569bbf33c
Sebastiaan van Stijn <github@gone.nl>
2025-08-28 14:33:21 +0200

    cli/command/stack/swarm: remove deprecated RunPS and options.PS
    These were deprecated in f0e5a0d6545399477660087e2db69ebbf831666d and
    036d3a6bab54fdffd9804ab2367fb9a14e62893b and were only used internally.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/stack/options/opts.go
+++ cli/command/stack/options/opts.go
@@ -1,7 +1,5 @@
 package options
 
-import "github.com/docker/cli/opts"
-
 // Deploy holds docker stack deploy options
 //
 // Deprecated: this type was for internal use and will be removed in the next release.
@@ -21,18 +19,6 @@ type Deploy struct {
 type Config struct {
 	Composefiles      []string
 	SkipInterpolation bool
-}
-
-// PS holds docker stack ps options
-//
-// Deprecated: this type was for internal use and will be removed in the next release.
-type PS struct {
-	Filter    opts.FilterOpt
-	NoTrunc   bool
-	Namespace string
-	NoResolve bool
-	Quiet     bool
-	Format    string
 }
 
 // Remove holds docker stack remove options
