f0e5a0d6545399477660087e2db69ebbf831666d
Sebastiaan van Stijn <github@gone.nl>
2025-08-28 10:15:13 +0200

    deprecate cli/command/stack/options
    Functions and types in this package were exported as part of the "compose
    on kubernetes" feature, which was deprecated and removed. These functions
    are meant for internal use, and will be removed in the next release.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/stack/options/opts.go
+++ cli/command/stack/options/opts.go
@@ -3,6 +3,8 @@ package options
 import "github.com/docker/cli/opts"
 
 // Deploy holds docker stack deploy options
+//
+// Deprecated: this type was for internal use and will be removed in the next release.
 type Deploy struct {
 	Composefiles     []string
 	Namespace        string
@@ -14,18 +16,24 @@ type Deploy struct {
 }
 
 // Config holds docker stack config options
+//
+// Deprecated: this type was for internal use and will be removed in the next release.
 type Config struct {
 	Composefiles      []string
 	SkipInterpolation bool
 }
 
 // List holds docker stack ls options
+//
+// Deprecated: this type was for internal use and will be removed in the next release.
 type List struct {
 	Format        string
 	AllNamespaces bool
 }
 
 // PS holds docker stack ps options
+//
+// Deprecated: this type was for internal use and will be removed in the next release.
 type PS struct {
 	Filter    opts.FilterOpt
 	NoTrunc   bool
@@ -36,12 +44,16 @@ type PS struct {
 }
 
 // Remove holds docker stack remove options
+//
+// Deprecated: this type was for internal use and will be removed in the next release.
 type Remove struct {
 	Namespaces []string
 	Detach     bool
 }
 
 // Services holds docker stack services options
+//
+// Deprecated: this type was for internal use and will be removed in the next release.
 type Services struct {
 	Quiet     bool
 	Format    string
