95eeafa5514aea3ebe00b9f19c86c097a9eaf0b6
Sebastiaan van Stijn <github@gone.nl>
2025-08-30 00:44:43 +0200

    cli/command/context: deprecate exported types and functions
    These functions and types are shallow wrappers around the context
    store and were intended for internal use as implementation for the
    CLI itself.
    
    They were exported in 3126920af14ea5127e00a2f8f9d8e07a6c3f6ff9 to be
    used by plugins and Docker Desktop. However, there's currently no public
    uses of this, and Docker Desktop does not use these functions.
    
    This patch deprecates the exported functions as they were meant to be
    implementation specific for the CLI. If there's a need to provide
    utilities for manipulating the context-store other than through the
    CLI itself, we can consider creating an SDK for that purpose.
    
    This deprecates:
    
    - `RunCreate` and `CreateOptions`
    - `RunExport` and `ExportOptions`
    - `RunImport`
    - `RunRemove` and `RemoveOptions`
    - `RunUpdate` and `UpdateOptions`
    - `RunUse`
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/context/import.go
+++ cli/command/context/import.go
@@ -18,7 +18,7 @@ func newImportCommand(dockerCli command.Cli) *cobra.Command {
 		Short: "Import a context from a tar or zip file",
 		Args:  cli.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return RunImport(dockerCli, args[0], args[1])
+			return runImport(dockerCli, args[0], args[1])
 		},
 		// TODO(thaJeztah): this should also include "-"
 		ValidArgsFunction: completion.FileNames,
@@ -27,14 +27,21 @@ func newImportCommand(dockerCli command.Cli) *cobra.Command {
 }
 
 // RunImport imports a Docker context
-func RunImport(dockerCli command.Cli, name string, source string) error {
-	if err := checkContextNameForCreation(dockerCli.ContextStore(), name); err != nil {
+//
+// Deprecated: this function was for internal use and will be removed in the next release.
+func RunImport(dockerCLI command.Cli, name string, source string) error {
+	return runImport(dockerCLI, name, source)
+}
+
+// runImport imports a Docker context.
+func runImport(dockerCLI command.Cli, name string, source string) error {
+	if err := checkContextNameForCreation(dockerCLI.ContextStore(), name); err != nil {
 		return err
 	}
 
 	var reader io.Reader
 	if source == "-" {
-		reader = dockerCli.In()
+		reader = dockerCLI.In()
 	} else {
 		f, err := os.Open(source)
 		if err != nil {
@@ -44,11 +51,11 @@ func RunImport(dockerCli command.Cli, name string, source string) error {
 		reader = f
 	}
 
-	if err := store.Import(name, dockerCli.ContextStore(), reader); err != nil {
+	if err := store.Import(name, dockerCLI.ContextStore(), reader); err != nil {
 		return err
 	}
 
-	_, _ = fmt.Fprintln(dockerCli.Out(), name)
-	_, _ = fmt.Fprintf(dockerCli.Err(), "Successfully imported context %q\n", name)
+	_, _ = fmt.Fprintln(dockerCLI.Out(), name)
+	_, _ = fmt.Fprintf(dockerCLI.Err(), "Successfully imported context %q\n", name)
 	return nil
 }
