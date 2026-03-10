32144070e7701f91d03fb6edf73aa9c3259a7bde
Sebastiaan van Stijn <github@gone.nl>
2026-03-10 10:46:02 +0100

    WIP: separate hook types from manager
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

---
+++ cli-plugins/hooks/plugin_data.go
@@ -0,0 +1,16 @@
+package hooks
+
+// Data is the type representing the information
+// that plugins declaring support for hooks get passed when
+// being invoked following a CLI command execution.
+type Data struct {
+	// RootCmd is a string representing the matching hook configuration
+	// which is currently being invoked. If a hook for `docker context` is
+	// configured and the user executes `docker context ls`, the plugin will
+	// be invoked with `context`.
+	RootCmd string
+	Flags   map[string]string
+
+	// CommandError is a string containing the error output that p
+	CommandError string
+}
