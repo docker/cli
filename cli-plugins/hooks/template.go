dce201d6ee31cfd13beecb4942a130643b9af8bf
Sebastiaan van Stijn <github@gone.nl>
2026-03-18 01:27:31 +0100

    cli-plugins/hooks: move template utils separate from render code
    These utilities are used by CLI-plugins; separate them from the render
    code, which is used by teh CLI-plugin manager.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli-plugins/hooks/template.go
+++ cli-plugins/hooks/template.go
@@ -3,59 +3,11 @@ package hooks
 import (
 	"bytes"
 	"errors"
-	"fmt"
-	"strconv"
 	"strings"
 	"text/template"
 
 	"github.com/spf13/cobra"
 )
-
-// TemplateReplaceSubcommandName returns a hook template string
-// that will be replaced by the CLI subcommand being executed
-//
-// Example:
-//
-// "you ran the subcommand: " + TemplateReplaceSubcommandName()
-//
-// when being executed after the command:
-// `docker run --name "my-container" alpine`
-// will result in the message:
-// `you ran the subcommand: run`
-func TemplateReplaceSubcommandName() string {
-	return hookTemplateCommandName
-}
-
-// TemplateReplaceFlagValue returns a hook template string
-// that will be replaced by the flags value.
-//
-// Example:
-//
-// "you ran a container named: " + TemplateReplaceFlagValue("name")
-//
-// when being executed after the command:
-// `docker run --name "my-container" alpine`
-// will result in the message:
-// `you ran a container named: my-container`
-func TemplateReplaceFlagValue(flag string) string {
-	return fmt.Sprintf(hookTemplateFlagValue, flag)
-}
-
-// TemplateReplaceArg takes an index i and returns a hook
-// template string that the CLI will replace the template with
-// the ith argument, after processing the passed flags.
-//
-// Example:
-//
-// "run this image with `docker run " + TemplateReplaceArg(0) + "`"
-//
-// when being executed after the command:
-// `docker pull alpine`
-// will result in the message:
-// "Run this image with `docker run alpine`"
-func TemplateReplaceArg(i int) string {
-	return fmt.Sprintf(hookTemplateArg, strconv.Itoa(i))
-}
 
 func ParseTemplate(hookTemplate string, cmd *cobra.Command) ([]string, error) {
 	tmpl := template.New("").Funcs(commandFunctions)
@@ -72,12 +24,6 @@ func ParseTemplate(hookTemplate string, cmd *cobra.Command) ([]string, error) {
 }
 
 var ErrHookTemplateParse = errors.New("failed to parse hook template")
-
-const (
-	hookTemplateCommandName = "{{.Name}}"
-	hookTemplateFlagValue   = `{{flag . "%s"}}`
-	hookTemplateArg         = "{{arg . %s}}"
-)
 
 var commandFunctions = template.FuncMap{
 	"flag": getFlagValue,
